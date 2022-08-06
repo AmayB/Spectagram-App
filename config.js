05e9081818c83ef60ce66e870d9922b4adf85ca5
AmayB <87885769+AmayB@users.noreply.github.com>
2022-08-06 09:22:02 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQdtFbKUUdQga0y7Lgyo1g0jBQTnI7TMY",
+  authDomain: "spectagram-7780b.firebaseapp.com",
+  projectId: "spectagram-7780b",
+  storageBucket: "spectagram-7780b.appspot.com",
+  messagingSenderId: "373561626020",
+  appId: "1:373561626020:web:83579c4893ef476cb0c531"
+};
